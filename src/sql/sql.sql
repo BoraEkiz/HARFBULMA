37e532a960c14146a61329add89be35c407f23f3
BoraEkiz <Boraekiz70@gmail.com>
2025-06-15 23:07:14 +0300

    .

---
+++ src/sql/sql.sql
@@ -0,0 +1,35 @@
+SELECT
+    city.city_name AS city,
+    country.country_name AS country
+FROM
+    city
+INNER JOIN
+    country
+ON
+    city.country_id = country.country_id;
+
+
+
+SELECT
+    payment.payment_id,
+    customer.first_name,
+    customer.last_name
+FROM
+    customer
+INNER JOIN
+    payment
+ON
+    customer.customer_id = payment.customer_id;
+
+
+SELECT
+    rental.rental_id,
+    customer.first_name,
+    customer.last_name
+FROM
+    customer
+INNER JOIN
+    rental
+ON
+    customer.customer_id = rental.customer_id;
+
